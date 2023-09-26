a6c87b4a4c21f367fc7bb2672865301c326f4d88
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2023-09-26 14:32:58 -0300

    feat: created card component

---
+++ src/components/CardNew.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container-card">
+    <div class="card-image">
+      <img
+        src="https://images.unsplash.com/photo-1547981609-4b6bfe67ca0b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+      />
+    </div>
+    <div class="card-content">
+      <h3>China's cutthroat competition hasn't been lost with the slowdown</h3>
+
+      <p>
+        At least six Chinese electric car companies made product announcements in the last week.
+      </p>
+
+      <span>Published: </span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container-card {
+  width: 13rem;
+}
+
+.card-content {
+  > p,
+  span {
+    color: var(--color-gray-200);
+    margin-top: 1rem;
+    display: block;
+  }
+}
+.card-image {
+  > img {
+    width: 210px;
+    height: 130px;
+    border-radius: 1rem;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -17,10 +17,20 @@
       />
       <CustomButton>Search</CustomButton>
     </div>
+
+    <div class="container-cards">
+      <h3>Last News</h3>
+      <div class="cards">
+        <CardNew />
+        <CardNew />
+        <CardNew />
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
+import CardNew from '../components/CardNew.vue'
 import CustomButton from '../components/CustomButton.vue'
 import TextField from '../components/TextField.vue'
 import { ref } from 'vue'
@@ -29,6 +39,20 @@ const query = ref('')
 </script>
 
 <style scoped>
+.container-cards {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+  margin-top: 5rem;
+}
+
+.cards {
+  display: flex;
+  gap: 2rem;
+  flex-wrap: wrap;
+  justify-content: center;
+}
 .search {
   text-align: center;
   align-items: center;
