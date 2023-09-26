0cfaef808564c0ea8b3eb827d50323277f5912c7
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2023-09-26 13:40:44 -0300

    feat: create custom button

---
+++ src/components/CustomButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <button @click="onClick">
+    <slot></slot>
+  </button>
+</template>
+
+<script setup>
+const emit = defineEmits(['click'])
+
+const onClick = (event) => emit('click', event)
+</script>
+
+<style scoped>
+button {
+  background-color: var(--color-primary-base);
+  color: var(--color-gray-100);
+  border: 1px solid var(--color-primary-base);
+  padding: 0.5rem 1rem;
+  border-radius: 1rem;
+  cursor: pointer;
+  border-radius: 3rem;
+  min-width: 6rem;
+
+  &:hover {
+    background-color: transparent;
+    color: var(--color-primary-base);
+  }
+}
+</style>
