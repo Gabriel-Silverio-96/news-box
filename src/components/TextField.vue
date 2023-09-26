2333aefa49f796f83d4b8aad8128930554e34ae8
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2023-09-26 12:51:42 -0300

    feat: create custom input

---
+++ src/components/TextField.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <label :for="id">{{ label }}</label>
+    <input :id="id" v-model="modelValue" :placeholder="placeholder" @input="onInput" />
+  </div>
+</template>
+
+<script setup>
+import { ref, defineProps, defineEmits } from 'vue'
+
+const { label, id, placeholder, value } = defineProps({
+  label: String,
+  id: [String, Number],
+  placeholder: String,
+  value: String
+})
+
+const modelValue = ref(value)
+
+const emit = defineEmits(['update:modelValue'])
+const onInput = (event) => emit('update:modelValue', event.target.value)
+</script>
+
+<style scoped>
+input {
+  padding: 0.5rem 1rem;
+  border-radius: 3rem;
+  width: auto;
+  border: 1.4px solid var(--color-gray-200);
+  outline: none;
+
+  &:focus {
+    border-color: var(--color-primary-base);
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,42 @@
 <template>
-  <img src="@/assets/logo.svg" />
+  <div class="container-home">
+    <div class="search">
+      <img src="@/assets/logo.svg" width="100" height="50" />
 
-  <h1>
-    What's the new <br> you're looking for?
-  </h1>
+      <h1>
+        What's the new <br />
+        you're looking for?
+      </h1>
+
+      <TextField
+        id="query"
+        type="text"
+        v-model="query"
+        :value="query"
+        placeholder="Ex: Technology Trends"
+      />
+    </div>
+  </div>
+
+  {{ query }}
 </template>
+
+<script setup>
+import TextField from '../components/TextField.vue'
+import { ref } from 'vue'
+
+const query = ref('')
+</script>
+
+<style scoped>
+.container-home {
+}
+
+.search {
+  text-align: center;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+</style>
