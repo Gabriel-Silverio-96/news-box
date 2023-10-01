5cfe428a2ee380a0f44f4a279d572920553ad24f
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2023-10-01 06:09:47 -0300

    test: AlertComponent

---
+++ src/components/__tests__/AlertComponent.test.js
@@ -0,0 +1,58 @@
+import { mount } from "@vue/test-utils";
+import { describe, it, expect } from "vitest";
+import AlertComponent from "../AlertComponent.vue"
+
+const TESTS_CASES = [
+    {
+        message: 'This is an error message',
+        severity: 'error',
+        expectedClass: 'alert-error',
+    },
+    {
+        message: 'This is a warning message',
+        severity: 'warning',
+        expectedClass: 'alert-warning',
+    },
+    {
+        message: 'This is a info message',
+        severity: 'info',
+        expectedClass: 'alert-info',
+    },
+    {
+        message: 'This is a success message',
+        severity: 'success',
+        expectedClass: 'alert-success',
+    }
+]
+
+
+describe("AlertComponent", () => {
+    it("Render component", () => {
+        const component = mount(AlertComponent, {
+            props: {
+                message: 'This is an alert message',
+                severity: 'error',
+                show: true,
+            },
+        });
+
+        expect(component.exists()).toBeTruthy()
+    })
+
+    it.each(TESTS_CASES)(
+        'Should show the alert with the defined message, severity and show properties',
+        ({ message, severity, expectedClass }) => {
+            const component = mount(AlertComponent, {
+                props: {
+                    message,
+                    severity,
+                    show: true,
+                },
+            });
+
+            expect(component.exists()).toBeTruthy()
+            expect(component.text()).toContain(message);
+            expect(component.classes()).toContain(expectedClass);
+        }
+    );
+})
