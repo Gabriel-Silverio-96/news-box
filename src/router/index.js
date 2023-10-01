b95263a261a093587e4c8b6950c08e6ced5f90fc
Gabriel Silvério <53228013+Gabriel-Silverio-96@users.noreply.github.com>
2023-10-01 05:46:07 -0300

    Revert "feat: create pages home and result search"

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import ResultSearchView from '../views/ResultSearchView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,9 +10,12 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/result-search',
-      name: 'result-search',
-      component: ResultSearchView
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
     }
   ]
 })
