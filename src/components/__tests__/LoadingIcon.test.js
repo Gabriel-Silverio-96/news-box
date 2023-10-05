40e6a5067e6a8d2b23faa2f06f40c09accfbfab3
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2023-10-05 19:53:19 -0300

    test: LoadingIcon

---
+++ src/components/__tests__/LoadingIcon.test.js
@@ -0,0 +1,36 @@
+import { mount } from "@vue/test-utils";
+import { describe, expect, it } from "vitest";
+import LoadingIcon from '@/components/LoadingIcon.vue'
+
+describe('LoadingIcon.vue', () => {
+    it('mmont component', () => {
+        const wrapper = mount(LoadingIcon)
+
+        expect(wrapper.exists()).toBe(true)
+    })
+
+    it('unmmont component', () => {
+        const wrapper = mount(LoadingIcon)
+
+        wrapper.unmount()
+        expect(wrapper.exists()).toBe(false)
+    })
+
+    it('should not be visible when isLoading is false', () => {
+        const wrapper = mount(LoadingIcon, { props: { isLoading: false } })
+
+        expect(wrapper.isVisible()).toBe(false)
+    })
+
+    it('should correctly render component with defined props', () => {
+        const message = 'message loading'
+        const width = '50'
+        const height = '45'
+
+        const wrapper = mount(LoadingIcon, { props: { message, width, height } })
+
+        expect(wrapper.text()).toBe(message)
+        expect(wrapper.find('img').attributes('width')).toBe(width)
+        expect(wrapper.find('img').attributes('height')).toBe(height)
+    })
+})
